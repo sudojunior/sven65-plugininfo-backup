37d2b1478490514c4d5878a223371bc5b5533c07
Junior <PlayTheFallen@gmail.com>
2020-06-02 19:55:35 +0100

    update(script): 3 changes, new config key.
    * Add 'updateEmbed' config key.
    * Whitespace is now tab intents.
    * Remove async from payload function.

--- post.js
+++ post.js
@@ -1,14 +1,17 @@
-const { promises: fs } = require('fs');
+/* eslint-disable indent */
+const fs = require('fs/promises');
 const path = require('path');
 
 const {
-  WebhookClient,
-  Util: { mergeDefault },
+	WebhookClient,
+	Util: { mergeDefault, resolveColor },
 } = require('discord.js');
 
 const config = require('./config.json');
 
-const webhook = new WebhookClient(config.webhook.id, config.webhook.token);
+const webhook = new WebhookClient(config.webhook.id, config.webhook.token, {
+	disableMentions: 'none',
+});
 
 /**
  *
@@ -17,7 +20,7 @@ const webhook = new WebhookClient(config.webhook.id, config.webhook.token);
  */
 const sleep = (time = 1000) => new Promise((resolve) => setTimeout(() => resolve()), time);
 
-const sendPayload = async (payload) => webhook.send(mergeDefault(config.defaultPayload, payload));
+const sendPayload = (payload) => webhook.send(mergeDefault(config.defaultPayload, payload));
 
 /**
  *
@@ -25,61 +28,65 @@ const sendPayload = async (payload) => webhook.send(mergeDefault(config.defaultP
  * @returns {Promise<[Buffer, path.ParsedPath]>}
  */
 async function getFile(filePath) {
-  filePath = path.resolve(filePath);
-  const fileInfo = path.parse(filePath);
-  const fileContent = await fs.readFile(filePath);
+	filePath = path.resolve(filePath);
+	const fileInfo = path.parse(filePath);
+	const fileContent = await fs.readFile(filePath);
 
-  return [fileContent, fileInfo];
+	return [fileContent, fileInfo];
 }
 
 const run = async () => {
-  for (let index = 0; index < config.files.length; index++) {
-    const [content, info] = await getFile(config.files[index]);
+	for (let index = 0; index < config.files.length; index++) {
+		const [content, info] = await getFile(config.files[index]);
 
-    let payload;
-    switch (info.ext) {
-      case '.png': {
-        payload = {
-          files: [{
-            attachment: content,
-            name: `${info.name}.${info.ext}`,
-          }],
-        };
-        break;
-      }
-      case '.txt':
-      case '.md': {
-        // sendPayload({ embeds: [{ description: content.toString("utf8") }] });
-        payload = { content: content.toString('utf8') };
-        break;
-      }
-      case '.json': {
-        payload = JSON.parse(content.toString('utf8'));
-        break;
-      }
-      default: {
-        continue;
-      }
-    }
+		let payload;
+		switch (info.ext) {
+			case '.png': {
+				payload = {
+					files: [{
+						attachment: content,
+						name: `${info.name}.${info.ext}`,
+					}],
+				};
+				break;
+			}
+			case '.txt':
+			case '.md': {
+				// sendPayload({ embeds: [{ description: content.toString("utf8") }] });
+				payload = { content: content.toString('utf8') };
+				break;
+			}
+			case '.json': {
+				payload = JSON.parse(content.toString('utf8'));
+				break;
+			}
+			default: {
+				continue;
+			}
+		}
 
-    await sendPayload(payload);
+		await sendPayload(payload);
 
-    console.log(`[${index}] Sent content for ${info.name}${info.ext}`);
+		console.log(`[${index}] Sent content for ${info.name}${info.ext}`);
 
-    await sleep(2000);
-  }
-  const stamp = new Date();
-  await sendPayload({
-    embeds: [
-      {
-        description: `This channel was last updated ${stamp.toUTCString()} (${stamp.toISOString()})`,
-        color: 8548838,
-      },
-    ],
-  });
+		await sleep(2000);
+	}
+	const stamp = new Date();
+	if (config.updatePayload && config.updatePayload.length > 0) {
+		await sendPayload(mergeDefault({
+			embeds: [
+				{
+					description: 'This channel has been backed up using [TinkerStorm/discord-channel-backup](https://github.com/TinkerStorm/discord-channel-backup).',
+					color: resolveColor('#7289DA'),
+					timestamp: stamp,
+				},
+			],
+		}, config.updatedEmbed));
+		console.log('[*] Sent content for update embed.');
+	}
 };
 
 run().then(() => {
-  console.log('Script run complete');
-  process.exit(0);
+	console.log('Script run complete');
+	process.exit(0);
 });
